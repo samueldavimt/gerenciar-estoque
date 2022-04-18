0c807ed66fe454b02f94b2bcae90d8c416a38aed
Samuel Davi <samuel.davimont@gmail.com>
2022-04-18 02:35:42 -0300

    layout do header

--- logout.php
+++ logout.php
@@ -5,3 +5,5 @@ session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
+
+header("Location: index.php");exit;
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,13 @@
 
     require_once("config/globals.php");
     require_once("vendor/autoload.php");
+
+
+    use \classes\Dao\UserDAOMysql;
+
+    $userDao = new UserDAOMysql($pdo);
+
+    $userData = $userDao->verifyToken();
     
 ?>
 
@@ -17,4 +24,53 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="<?=$BASE_URL?>">Gerenciamento</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        
+                        <?php if(!$userData):?>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="<?=$BASE_URL?>">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?=$BASE_URL?>contact.php">Contato</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?=$BASE_URL?>login.php">Login</a>
+                            </li>
+                        <?php else:?>
+                           
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link active dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <?=$userData->getName()?>
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a class="dropdown-item" href="#">Minha Conta</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" href="<?=$BASE_URL?>logout.php">Logout</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="<?=$BASE_URL?>">Painel</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="<?=$BASE_URL?>contact.php">Estoque</a>
+                            </li>
+                        
+                        <?php endif?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
     
